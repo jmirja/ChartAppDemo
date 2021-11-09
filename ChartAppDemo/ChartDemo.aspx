c51c363cabcde97ef252615afce70b5acbbb07b8
jmirja <shohan.mirja@gmail.com>
2021-11-10 02:55:28 +0600

    Added chart data programmatically in ChartDemo.aspx

--- ChartAppDemo/ChartDemo.aspx
+++ ChartAppDemo/ChartDemo.aspx
@@ -1,3 +1,38 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ChartDemo.aspx.cs" Inherits="ChartAppDemo.ChartDemo" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <table style="border: 1px solid black; font-family: Arial">
+<tr>
+    <td>
+        <b>Select Chart Type:</b>
+    </td>
+    <td>
+        <asp:DropDownList ID="DropDownList1" AutoPostBack="true" runat="server"
+            OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+    </td>
+</tr>
+<tr>
+    <td colspan="2">
+        <asp:Chart ID="Chart1" runat="server" Width="350px">
+            <Titles>
+                <asp:Title Text="Total marks of students">
+                </asp:Title>
+            </Titles>
+            <Series>
+                <asp:Series Name="Series1" ChartArea="ChartArea1" ChartType="Pie">
+                </asp:Series>
+            </Series>
+            <ChartAreas>
+                <asp:ChartArea Name="ChartArea1">
+                    <AxisX Title="Student Name">
+                    </AxisX>
+                    <AxisY Title="Total Marks">
+                    </AxisY>
+                </asp:ChartArea>
+            </ChartAreas>
+        </asp:Chart>
+    </td>
+</tr>
+</table>
 </asp:Content>
--- ChartAppDemo/ChartDemo.aspx.cs
+++ ChartAppDemo/ChartDemo.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.DataVisualization.Charting;
 using System.Web.UI.WebControls;
 
 namespace ChartAppDemo
@@ -11,7 +12,43 @@ namespace ChartAppDemo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                // Call Get ChartData() method in the PageLoad event
+                GetChartData();
+                GetChartTypes();
+            }
+        }
 
+        private void GetChartData()
+        {
+            // Retrieve the Series to which we want to add DataPoints
+            Series series = Chart1.Series["Series1"];
+            // Add X and Y values using AddXY() method
+            series.Points.AddXY("Mark", 800);
+            series.Points.AddXY("Steve", 900);
+            series.Points.AddXY("John", 700);
+            series.Points.AddXY("Mary", 900);
+            series.Points.AddXY("Ben", 600);
+        }
+
+        private void GetChartTypes()
+        {
+            foreach (int chartType in Enum.GetValues(typeof(SeriesChartType)))
+            {
+                ListItem li = new ListItem(Enum.GetName(typeof(SeriesChartType),
+                    chartType), chartType.ToString());
+                DropDownList1.Items.Add(li);
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Call Get ChartData() method when the user select a different chart type
+            GetChartData();
+
+            this.Chart1.Series["Series1"].ChartType = (SeriesChartType)Enum.Parse(
+                typeof(SeriesChartType), DropDownList1.SelectedValue);
         }
     }
 }
