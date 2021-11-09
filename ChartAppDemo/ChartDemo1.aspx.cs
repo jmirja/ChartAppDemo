35fc4476d4e274e87109c80fb890390caadcdb0b
jmirja <shohan.mirja@gmail.com>
2021-11-10 03:51:19 +0600

    bind data from database to chart control

---
+++ ChartAppDemo/ChartDemo1.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.DataVisualization.Charting;
+using System.Web.UI.WebControls;
+
+namespace ChartAppDemo
+{
+    public partial class ChartDemo1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                
+                GetChartData();
+                GetChartTypes();
+            }
+
+        }
+
+        private void GetChartData()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["CS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                // Command to retrieve Students data from Students table
+                SqlCommand cmd = new
+                    SqlCommand("Select StudentName, TotalMarks from Students", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                // Retrieve the Series to which we want to add DataPoints
+                Series series = Chart1.Series["Series1"];
+                // Loop thru each Student record
+                while (rdr.Read())
+                {
+                    // Add X and Y values using AddXY() method
+                    series.Points.AddXY(rdr["StudentName"].ToString(),rdr["TotalMarks"]);
+                }
+            }
+        }
+
+        private void GetChartTypes()
+        {
+            foreach (int chartType in Enum.GetValues(typeof(SeriesChartType)))
+            {
+                ListItem li = new ListItem(Enum.GetName(typeof(SeriesChartType),
+                    chartType), chartType.ToString());
+                DropDownList1.Items.Add(li);
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetChartData();
+
+            this.Chart1.Series["Series1"].ChartType = (SeriesChartType)Enum.Parse(
+                typeof(SeriesChartType), DropDownList1.SelectedValue);
+        }
+    }
+}
