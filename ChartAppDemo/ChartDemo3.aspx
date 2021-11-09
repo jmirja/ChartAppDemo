ea3a0255339fac7addb27758ae39618dfcd1bc33
jmirja <shohan.mirja@gmail.com>
2021-11-10 04:17:27 +0600

    binding data to chart control using DataBindTable() method.

---
+++ ChartAppDemo/ChartDemo3.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ChartDemo3.aspx.cs" Inherits="ChartAppDemo.ChartDemo3" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <table style="border: 1px solid black; font-family: Arial">
+<tr>
+    <td>
+        <b>Select Chart Type:</b>
+    </td>
+    <td>
+        <asp:DropDownList ID="DropDownList1" AutoPostBack="true" runat="server"
+            OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+    </td>
+</tr>
+<tr>
+    <td colspan="2">
+        <asp:Chart ID="Chart1" runat="server" Width="350px">
+            <Titles>
+                <asp:Title Text="Total marks of students">
+                </asp:Title>
+            </Titles>
+            <Series>
+            </Series>
+            <ChartAreas>
+                <asp:ChartArea Name="ChartArea1">
+                    <AxisX Title="Student Name">
+                    </AxisX>
+                    <AxisY Title="Total Marks">
+                    </AxisY>
+                </asp:ChartArea>
+            </ChartAreas>
+        </asp:Chart>
+    </td>
+</tr>
+</table>
+</asp:Content>
---
+++ ChartAppDemo/ChartDemo3.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.DataVisualization.Charting;
+using System.Web.UI.WebControls;
+
+namespace ChartAppDemo
+{
+    public partial class ChartDemo3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+
+                GetChartData();
+                GetChartTypes();
+            }
+        }
+
+        private void GetChartData()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["CS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select StudentName, TotalMarks from Students", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                // Pass the datareader object that contains the chart data and
+                // specify the column to be used for X-AXIS values. The other
+                // column values will be automatically used for Y-AXIS
+                Chart1.DataBindTable(rdr, "StudentName");
+            }
+        }
+
+        private void GetChartTypes()
+        {
+            foreach (int chartType in Enum.GetValues(typeof(SeriesChartType)))
+            {
+                ListItem li = new ListItem(Enum.GetName(typeof(SeriesChartType),
+                    chartType), chartType.ToString());
+                DropDownList1.Items.Add(li);
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetChartData();
+            // Retrieve the series by index and then
+            // set the ChartType property value
+            this.Chart1.Series[0].ChartType = (SeriesChartType)Enum.Parse(typeof(SeriesChartType), DropDownList1.SelectedValue);
+
+            // Alternatively, FirstOrDefault() linq method can also be used
+
+
+            // this.Chart1.Series.FirstOrDefault().ChartType = (SeriesChartType)
+            // Enum.Parse(typeof(SeriesChartType), DropDownList1.SelectedValue);
+        }
+    }
+}
